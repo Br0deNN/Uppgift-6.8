e9f610c4116e500053a8106d469c980ac9965ebf
7R1XY <theo.broden@gmail.com>
2024-02-16 13:11:21 +0100

    Add project files.

---
+++ Uppgift 6.8/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Skriv in ett tal du vill undersöka");
+        int x = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Antalet primtal mindre än {x} är {Mindreän(x)}");
+
+
+    }
+    static bool Primtal(int x)
+    {
+        int delx = 0;
+        for (int i = 2; i < x; i++)
+        {
+            if (x % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+        
+    }
+    static int Mindreän(int y)
+    {
+        int antal = 0;
+
+        for (int i = 2; i < y; i++)
+        {
+            if (Primtal(i))
+            {
+                antal++;
+            }
+        }
+
+        return antal;
+    }
+}
